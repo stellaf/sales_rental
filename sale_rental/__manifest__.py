a0c04e537fb7d7538c1636dd960e113d9aa60f3a
stellaf <stella.fredo@gmail.com>
2017-04-15 22:31:23 +0200

    Add files via upload
    Sales Rental module for Odoo 10

---
+++ sale_rental/__manifest__.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+# Copyright 2014-2016 Akretion (http://www.akretion.com)
+# @author Alexis de Lattre <alexis.delattre@akretion.com>
+# Copyright 2016 Sodexis (http://sodexis.com)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Rental',
+    'version': '10.0.1.0.0',
+    'category': 'Sales Management',
+    'license': 'AGPL-3',
+    'summary': 'Manage Rental of Products',
+    'author': 'Akretion, Sodexis, Odoo Community Association (OCA)',
+    'website': 'http://www.akretion.com',
+    'depends': ['sale_start_end_dates', 'sale_stock', 'sales_team'],
+    'data': [
+        'data/rental_data.xml',
+        'views/sale_order.xml',
+        'views/stock.xml',
+        'views/sale_rental.xml',
+        'wizard/create_rental_product_view.xml',
+        'views/product.xml',
+        'security/ir.model.access.csv',
+        'security/sale_rental_security.xml',
+    ],
+    'demo': ['demo/rental_demo.xml'],
+    'installable': True,
+}
